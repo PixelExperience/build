3077cabc6edc83951604d18ce5589134c3fc158f
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-11 17:24:03 -0200

    Show ROM info while flashing
    Change-Id: I0769a74d70621536d3794bcdc3e70847945f00b5

--- tools/releasetools/non_ab_ota.py
+++ tools/releasetools/non_ab_ota.py
@@ -221,6 +221,17 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("Target: {}".format(target_info.fingerprint))
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
+
+  is_plus = target_info.GetBuildProp("org.pixelexperience.version").endswith(
+        "_plus")
+  android_version = target_info.GetBuildProp("ro.build.version.release")
+  build_id = target_info.GetBuildProp("ro.build.id")
+  build_date = target_info.GetBuildProp("org.pixelexperience.build_date")
+  security_patch = target_info.GetBuildProp("ro.build.version.security_patch")
+  device = target_info.GetBuildProp("org.pixelexperience.device")
+  script.PrintPixelExperienceBanner(is_plus, android_version, build_id, build_date,
+                                  security_patch, device)
+
   device_specific.FullOTA_InstallBegin()
 
   CopyInstallTools(output_zip)
