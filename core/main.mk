5c7febcea94c5953c847f92a0ec80c60e94b8386
Dyneteve <sound0020@gmail.com>
2019-12-02 18:06:50 +0200

    build: Move custom properties to product
    Change-Id: I779ed497358d6096de287682446e647546e1f678

--- core/main.mk
+++ core/main.mk
@@ -179,8 +179,8 @@ endif
 
 #
 # -----------------------------------------------------------------
-# Add our custom properties to the build properties.
-ADDITIONAL_BUILD_PROPERTIES += $(CUSTOM_PROPERTIES)
+# Add our custom properties to the product properties.
+ADDITIONAL_PRODUCT_PROPERTIES += $(CUSTOM_PROPERTIES)
 
 # Bring in standard build system definitions.
 include $(BUILD_SYSTEM)/definitions.mk
