2e7604862e80ecb22e25321bfa4d8e37eac7e1ed
Dyneteve <dyneteve@pixelexperience.org>
2019-12-11 19:13:34 +0200

    build: Use CUSTOM_BUILD for OTA prop
    Change-Id: I3e417ff98af75927a1f366616363b49d5c356339

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -55,6 +55,6 @@ if [ -n "$BUILD_THUMBPRINT" ] ; then
   echo "ro.build.thumbprint=$BUILD_THUMBPRINT"
 fi
 
-echo "org.pixelexperience.device=$CUSTOM_DEVICE"
+echo "org.pixelexperience.device=$CUSTOM_BUILD"
 
 echo "# end build properties"
