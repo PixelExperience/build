a0b827a2036ac11b92c954488fe4045dada3850e
Dyneteve <dyneteve@pixelexperience.org>
2019-12-22 21:39:41 +0200

    build: Move sound related props to pixelstyle
    Change-Id: I94c7ff482dc48eed660dc0d3747b6536d0ecee41

--- target/product/gsi_common.mk
+++ target/product/gsi_common.mk
@@ -30,11 +30,6 @@ endif
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.apex.updatable=false
 
-# Additional settings used in all AOSP builds
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg \
-
 # The mainline checking whitelist, should be clean up
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_WHITELIST += \
     system/app/messaging/messaging.apk \
