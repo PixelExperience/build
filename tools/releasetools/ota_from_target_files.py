08cdfbcae966b28ae3ba96319248465325c0132e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-07-08 19:41:39 -0300

    build: Revert backup tool commits
    Revert "releasetools: Disable backuptools for now"
    
    This reverts commit 86d23e7c721f80e7e7f4197e47e6c634ca8c1baa.
    
    Revert "releasetools: Move system mount handling to backuptool"
    
    This reverts commit fc5870537a93c07aa985066436cb44eb83402317.
    
    Revert "releasetools: squash backuptool support"
    
    This reverts commit 06dac26707566c5708058a805076884b27e84198.
    
    Change-Id: I0f8b0b93e56f15d93855a9e033f0cd23aa4781df

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -184,10 +184,6 @@ A/B OTA specific options
   --incremental_block_based <boolean>
       Enable block based incremental updates
       Disabled by default.
-
-  --backup <boolean>
-      Enable or disable the execution of backuptool.sh.
-      Disabled by default.
 """
 
 from __future__ import print_function
@@ -250,7 +246,6 @@ OPTIONS.retrofit_dynamic_partitions = False
 OPTIONS.skip_compatibility_check = False
 OPTIONS.output_metadata_path = None
 OPTIONS.override_device = 'auto'
-OPTIONS.backuptool = False
 
 
 METADATA_NAME = 'META-INF/com/android/metadata'
@@ -1391,21 +1386,12 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print(" Security patch: %s"%(security_patch));
   script.Print(" Device: %s"%(device));
   script.Print("----------------------------------------------");
-
   device_specific.FullOTA_InstallBegin()
 
   CopyInstallTools(output_zip)
   script.UnpackPackageDir("install", "/tmp/install")
   script.SetPermissionsRecursive("/tmp/install", 0, 0, 0o755, 0o644, None, None)
   script.SetPermissionsRecursive("/tmp/install/bin", 0, 0, 0o755, 0o755, None, None)
-
-  if target_info.get("system_root_image") == "true":
-    sysmount = "/"
-  else:
-    sysmount = "/system"
-
-  if OPTIONS.backuptool:
-    script.RunBackup("backup", sysmount)
 
   # All other partitions as well as the data wipe use 10% of the progress, and
   # the update of the system partition takes the remaining progress.
@@ -1437,10 +1423,6 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   common.ZipWriteStr(output_zip, "boot.img", boot_img.data)
 
   device_specific.FullOTA_PostValidate()
-
-  if OPTIONS.backuptool:
-    script.ShowProgress(0.02, 10)
-    script.RunBackup("restore", sysmount)
 
   script.WriteRawImage("/boot", "boot.img")
 
@@ -2890,8 +2872,6 @@ def main(argv):
       OPTIONS.override_device = a
     elif o in ("--incremental_block_based"):
       OPTIONS.incremental_block_based = bool(a.lower() == 'true')
-    elif o in ("--backup"):
-      OPTIONS.backuptool = bool(a.lower() == 'true')
     else:
       return False
     return True
@@ -2928,7 +2908,6 @@ def main(argv):
                                  "output_metadata_path=",
                                  "override_device=",
                                  "incremental_block_based=",
-                                 "backup=",
                              ], extra_option_handler=option_handler)
 
   if len(args) != 2:
