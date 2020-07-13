a98e82ea1a3f23c42d0645b7b503e19be11003ea
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-07-13 19:24:21 -0300

    build: Revert old incremental commits
    Revert "Cleanup flags for incremental ota and aosp recovery"
    
    This reverts commit 796cf02fb957d4337a56c18a03a3153a44a71ab7.
    
    Revert "Fix incremental generation after 086144f4ddc9b1689e7cfddedf7796219420b2b8"
    
    This reverts commit ac2511776261a7808b4f5ff48b8299f0dad3cf25.
    
    Revert "build: incrementals: Refactor source version checking"
    
    This reverts commit 802cbad4da42d7611a15e2ed2eee8c876819b4d4.
    
    Revert "build: Add support for install tools in incremental"
    
    This reverts commit 6f374aac71526553b224612dad6cabaa3f232a63.
    
    Revert "releasetools: Add support to enable block based incremental back"
    
    This reverts commit 84e0ded0144102979e5df1a1c3b74274ecbc8713.
    
    Change-Id: I24ebfdea79deeb43d08638ed7738f9f9fdcbec88
    
    Revert "build: Add back unmount system before flashing"
    
    This reverts commit 06cbf753c54a1063269fe50e1882e7f22024edbf.
    
    Revert "build: Fix mount points of incremental package script"
    
    This reverts commit 1d66842028402ffc00e0a89a3d3e60789244cb99.
    
    Revert "build: Cleanup edify functions"
    
    This reverts commit 9c96675c44177d42be4a7569d7ffa0f481999971.
    
    Change-Id: Ia60409179be4f65a1998ee38b8cc49630f0eb4ca
    
    Revert "build: Whitelist recovery-resource.dat"
    
    This reverts commit 8809c137283b646118b51e77f17ec40641906a96.
    
    Revert "build: Whitelist recovery files from incremental ota"
    
    This reverts commit e466b75e12ca90f45d12f1eed6cf14e70cc81a5e.
    
    Revert "build: Restore support for file based OTA"
    
    This reverts commit 8579c9a0bda54dae6b90ca789b33aaca62fc5a54.
    
    Change-Id: I776ba6ae5240b1393fba7816ac2bb320aad46925

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -31,14 +31,6 @@ class EdifyGenerator(object):
     else:
       self.fstab = fstab
 
-  def MakeTemporary(self):
-    """Make a temporary script object whose commands can latter be
-    appended to the parent script with AppendScript().  Used when the
-    caller wants to generate script commands out-of-order."""
-    x = EdifyGenerator(self.version, self.info)
-    x.mounts = self.mounts
-    return x
-
   @property
   def required_cache(self):
     """Return the minimum cache size to apply the update."""
@@ -203,11 +195,6 @@ class EdifyGenerator(object):
             target=target, source=source,
             code=common.ErrorCode.BAD_PATCH_FILE)))
 
-  def IncrementalFileBasedSourceVersionCheck(self, prop_path, source_version_prop, source_version, error_msg):
-    """Check that the given file has one of the
-    given sha1 hashes."""
-    self.script.append('assert(file_getprop("%s", "%s") == "%s" || abort("%s"));' % (prop_path, source_version_prop, source_version, error_msg))
-
   def CacheFreeSpaceCheck(self, amount):
     """Check that there's at least 'amount' space that can be made
     available on /cache."""
@@ -288,26 +275,6 @@ class EdifyGenerator(object):
 
     self.script.append('wipe_block_device("%s", %s);' % (device, size))
 
-  def DeleteFiles(self, file_list):
-    """Delete all files in file_list."""
-    if not file_list:
-      return
-    cmd = "delete(" + ",\0".join(['"%s"' % (i,) for i in file_list]) + ");"
-    self.script.append(self.WordWrap(cmd))
-
-  def DeleteFilesIfNotMatching(self, file_list):
-    """Delete the file in file_list if not matching the checksum."""
-    if not file_list:
-      return
-    for name, sha1 in file_list:
-      cmd = ('sha1_check(read_file("{name}"), "{sha1}") || '
-             'delete("{name}");'.format(name=name, sha1=sha1))
-      self.script.append(self.WordWrap(cmd))
-
-  def RenameFile(self, srcfile, tgtfile):
-    """Moves a file from one location to another."""
-    self.script.append('rename("%s", "%s");' % (srcfile, tgtfile))
-
   def ApplyPatch(self, srcfile, tgtfile, tgtsize, tgtsha1, *patchpairs):
     """Apply binary patches (in *patchpairs) to the given srcfile to
     produce tgtfile (which may be "-" to indicate overwriting the
@@ -350,17 +317,21 @@ class EdifyGenerator(object):
   def SetPermissionsRecursive(self, fn, uid, gid, dmode, fmode, selabel,
                               capabilities):
     """Recursively set path ownership and permissions."""
-    if capabilities is None:
-      capabilities = "0x0"
-    cmd = 'set_metadata_recursive("%s", "uid", %d, "gid", %d, ' \
-        '"dmode", 0%o, "fmode", 0%o' \
-        % (fn, uid, gid, dmode, fmode)
-    if not fn.startswith("/tmp"):
-      cmd += ', "capabilities", "%s"' % capabilities
-    if selabel is not None:
-      cmd += ', "selabel", "%s"' % selabel
-    cmd += ');'
-    self.script.append(cmd)
+    if not self.info.get("use_set_metadata", False):
+      self.script.append('set_perm_recursive(%d, %d, 0%o, 0%o, "%s");'
+                         % (uid, gid, dmode, fmode, fn))
+    else:
+      if capabilities is None:
+        capabilities = "0x0"
+      cmd = 'set_metadata_recursive("%s", "uid", %d, "gid", %d, ' \
+          '"dmode", 0%o, "fmode", 0%o' \
+          % (fn, uid, gid, dmode, fmode)
+      if not fn.startswith("/tmp"):
+        cmd += ', "capabilities", "%s"' % capabilities
+      if selabel is not None:
+        cmd += ', "selabel", "%s"' % selabel
+      cmd += ');'
+      self.script.append(cmd)
 
   def WriteRawImage(self, mount_point, fn, mapfn=None):
     """Write the given package file into the partition for the given
@@ -383,56 +354,13 @@ class EdifyGenerator(object):
         raise ValueError(
             "don't know how to write \"%s\" partitions" % p.fs_type)
 
-  def SetPermissions(self, fn, uid, gid, mode, selabel, capabilities):
-    """Set file ownership and permissions."""
-    if capabilities is None:
-      capabilities = "0x0"
-    cmd = 'set_metadata("%s", "uid", %d, "gid", %d, "mode", 0%o, ' \
-        '"capabilities", %s' % (fn, uid, gid, mode, capabilities)
-    if selabel is not None:
-      cmd += ', "selabel", "%s"' % selabel
-    cmd += ');'
-    self.script.append(cmd)
-
-  def SetPermissionsRecursive(self, fn, uid, gid, dmode, fmode, selabel,
-                              capabilities):
-    """Recursively set path ownership and permissions."""
-    if capabilities is None:
-      capabilities = "0x0"
-    cmd = 'set_metadata_recursive("%s", "uid", %d, "gid", %d, ' \
-        '"dmode", 0%o, "fmode", 0%o' \
-        % (fn, uid, gid, dmode, fmode)
-    if not fn.startswith("/tmp"):
-      cmd += ', "capabilities", "%s"' % capabilities
-    if selabel is not None:
-      cmd += ', "selabel", "%s"' % selabel
-    cmd += ');'
-    self.script.append(cmd)
-
-  def MakeSymlinks(self, symlink_list):
-    """Create symlinks, given a list of (dest, link) pairs."""
-    by_dest = {}
-    for d, l in symlink_list:
-      by_dest.setdefault(d, []).append(l)
-
-    for dest, links in sorted(by_dest.iteritems()):
-      cmd = ('symlink("%s", ' % (dest,) +
-             ",\0".join(['"' + i + '"' for i in sorted(links)]) + ");")
-      self.script.append(self.WordWrap(cmd))
-
   def AppendExtra(self, extra):
     """Append text verbatim to the output script."""
     self.script.append(extra)
 
-  def CheckAndUnmount(self, mount_point):
-    self.script.append('ifelse(is_mounted("{0}"), unmount("{0}"));'.format(mount_point))
-    if mount_point in self.mounts:
-      self.mounts.remove(mount_point)
-
   def Unmount(self, mount_point):
     self.script.append('unmount("%s");' % mount_point)
-    if mount_point in self.mounts:
-      self.mounts.remove(mount_point)
+    self.mounts.remove(mount_point)
 
   def UnmountAll(self):
     for p in sorted(self.mounts):
