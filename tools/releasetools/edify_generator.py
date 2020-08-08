0bf717fa7b97e9090885af7bee9f6a0d7ab1766d
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-08-08 12:45:56 -0300

    build: Add PixelExperience branding into incrementals
    * Always flash full boot.img instead of partial
    * Cleanup useless steps
    * Add PixelExperience assertion
    * Cleanup
    
    Change-Id: I7d52baa03d64a18d9588e67d6e666daa2d4cfe7b

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -244,8 +244,15 @@ class EdifyGenerator(object):
     """Log a message to the screen (if the logs are visible)."""
     self.script.append('ui_print("%s");' % (message,))
 
+  def AddPixelExperienceVersionAssertion(self, device, source_version):
+    error_msg = "Failed to apply update, please download full package at https://download.pixelexperience.org/" + device
+    prop_path = "/system/build.prop"
+    source_version_prop = "org.pixelexperience.version.display"
+    self.script.append('assert(file_getprop("%s", "%s") == "%s" || abort("%s"));' % (prop_path, source_version_prop, source_version, error_msg))
+
   def PrintPixelExperienceBanner(self, is_plus, android_version, build_id, build_date,
-                                  security_patch, device):
+                                  security_patch, device, prev_build_id=None,
+                                  prev_build_date=None, prev_security_patch=None):
     self.Print("----------------------------------------------")
     if is_plus:
       self.Print("        PixelExperience (Plus edition)")
@@ -254,9 +261,18 @@ class EdifyGenerator(object):
     self.Print("                by jhenrique09")
     self.Print("----------------------------------------------")
     self.Print(" Android version: %s"%(android_version))
-    self.Print(" Build id: %s"%(build_id))
-    self.Print(" Build date: %s"%(build_date))
-    self.Print(" Security patch: %s"%(security_patch))
+    if prev_build_id != None and prev_build_id != build_id:
+      self.Print(" Build id: %s -> %s"%(prev_build_id, build_id))
+    else:
+      self.Print(" Build id: %s"%(build_id))
+    if prev_build_date != None and prev_build_date != build_date:
+      self.Print(" Build date: %s -> %s"%(prev_build_date, build_date))
+    else:
+      self.Print(" Build date: %s"%(build_date))
+    if prev_security_patch != None and prev_security_patch != security_patch:
+      self.Print(" Security patch: %s -> %s"%(prev_security_patch, security_patch))
+    else:
+      self.Print(" Security patch: %s"%(security_patch))
     self.Print(" Device: %s"%(device))
     self.Print("----------------------------------------------")
 
