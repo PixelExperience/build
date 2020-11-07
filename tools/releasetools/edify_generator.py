ce277827a95c4af27c74eb62c9bf4908a634edea
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-07 13:38:07 -0300

    incrementals: Change mount points
    Change-Id: I750ae7dc6ce827c7746da32847a8f73d8a37ed9b

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -456,7 +456,7 @@ class EdifyGenerator(object):
     self.script.append('run_program("/sbin/sh", "/tmp/install/bin/umount_all.sh");')
 
   def AddPixelExperienceVersionAssertion(self, error_msg, source_version):
-    prop_path = "/system_root/system/build.prop"
+    prop_path = "/system/system/build.prop"
     source_version_prop = "org.pixelexperience.version.display"
     self.script.append('assert(try_file_getprop("%s", "%s") == "%s" || abort("%s"));' % (prop_path, source_version_prop, source_version, error_msg))
 
