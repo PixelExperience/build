92ee6e89420c55517823c22693017076d551b47f
jhenrique09 <jhsv09@gmail.com>
2021-10-09 23:31:54 -0300

    Remove su
    Change-Id: I8215272b0c462accf560efcf7ed5f1b5da3b1588

--- target/product/base_system.mk
+++ target/product/base_system.mk
@@ -396,11 +396,6 @@ PRODUCT_PACKAGES_DEBUG := \
     unwind_reg_info \
     unwind_symbols \
 
-ifeq ($(CUSTOM_BUILD),)
-PRODUCT_PACKAGES_DEBUG += \
-    su
-endif
-
 # The set of packages whose code can be loaded by the system server.
 PRODUCT_SYSTEM_SERVER_APPS += \
     SettingsProvider \
